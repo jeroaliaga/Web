fd3bf36b36db34e32a66dc3b5c5eb42807468951
jeroaliaga <jeronimoaliaga.dg@gmail.com>
2018-08-04 23:32:34 -0300

    Update

--- public_html/includes/contenidos/bloque_registro-login.php
+++ public_html/includes/contenidos/bloque_registro-login.php
@@ -45,42 +45,34 @@
 		  <form name="register_form" id="register_form" method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
 			  <div class="modal-body">
 				<div class="form-group width50">
-					<label for="email">Nombre*</label>
-					<input name="nombre" required type="text" class="form-control" <?php if($_SESSION['nombre']){echo 'value="'.$_SESSION['nombre'].'"';}?>>
+					<input name="nombre" required placeholder="Nombre *" type="text" class="form-control" <?php if($_SESSION['nombre']){echo 'value="'.$_SESSION['nombre'].'"';}?>>
 				</div>
 				<div class="form-group width50">
-					<label for="email">Apellido (Obligatorio para registro como profesional)</label>
-					<input name="apellido" type="text" class="form-control" <?php if($_SESSION['apellido']){echo 'value="'.$_SESSION['apellido'].'"';}?>>
+					<input name="apellido" placeholder="Apellido (Obligatorio para profesional)" type="text" class="form-control" <?php if($_SESSION['apellido']){echo 'value="'.$_SESSION['apellido'].'"';}?>>
 				</div>
 				<div class="form-group width50">
-					<label for="email">Email:</label>
-					<input name="email" required type="email" class="form-control" id="email" <?php if($_SESSION['email']){echo 'value="'.$_SESSION['email'].'"';}?>>
+					<input name="email" required placeholder="Email" type="email" class="form-control" id="email" <?php if($_SESSION['email']){echo 'value="'.$_SESSION['email'].'"';}?>>
 				</div>
 				<div class="form-group width50">
-					<label for="email">Repita su Email:</label>
-					<input name="reemail" required type="email" class="form-control" id="reemail" <?php if($_SESSION['reemail']){echo 'value="'.$_SESSION['reemail'].'"';}?>>
+					<input name="reemail" required placeholder="Repita su Email" type="email" class="form-control" id="reemail" <?php if($_SESSION['reemail']){echo 'value="'.$_SESSION['reemail'].'"';}?>>
 				</div>
 				<div class="form-group width50">
-					<label for="pwd">Contraseña:</label>
-					<input name="password" required type="password" class="form-control" id="pwd">
+					<input name="password" required placeholder="Contraseña" type="password" class="form-control" id="pwd">
 				</div>
 				<div class="form-group width50">
-					<label for="pwd">Repita su contraseña:</label>
-					<input name="repassword" required type="password" class="form-control" id="rpwd">
+					<input name="repassword" required placeholder="Repita su contraseña" type="password" class="form-control" id="rpwd">
 				</div>
 				<div class="form-group width50">
-					<label for="email">Tipo de registro*</label>
 					<select name="tipo_registro" class="form-control" required>
-						<option value='0'>Seleccionar</option>
+						<option value='0'>Tipo de registro</option>
 						<?php do { ?>
 						<option value="<?php echo $row_R_tipo_usuario['id']; ?>" <?php if($_SESSION['tipo_registro'] && $_SESSION['tipo_registro']==$row_R_tipo_usuario['id']){echo ' selected'; }?>><?php echo $row_R_tipo_usuario['tipo_usuario']; ?></option>
 						<?php } while ($row_R_tipo_usuario = mysql_fetch_assoc($R_tipo_usuario)); ?>
 					</select>
 				</div>
 			  </div>
-			  <div class="modal-footer">			
-				<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-				<button type="submit" class="btn btn-default">Continuar</button>
+			  <div class="modal-footer">
+				<button type="submit" class="btn btn-default">Registrarme</button>
 			  </div>
 			  <input type="hidden" name="action" value="register" />
 		  </form>
